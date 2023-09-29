bebcf2d606ccab13d246d05fa97b34e4f8b9f377
aym1king <49128030+aym1king@users.noreply.github.com>
2023-09-29 03:35:12 +0200

    switched to default parameters

--- src/transformer.py
+++ src/transformer.py
@@ -1,95 +1,59 @@
-
 # https://pytorch.org/docs/stable/_modules/torch/nn/modules/transformer.html#Transformer
+# https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import math
 import numpy as np
 
-"""
-input to encoding
-(batch size, sequence length, features) 
-- sequence length = number of time steps
-- features = each input time series
-
-transformer encoding block
-- layernormalization 
-- multiheadattention
-- layer dropout
-- feed forward part: layernorm, conv1d, dropout, conv1d
-
-final Mlp classification head
-- reduce output tensor of transforr encoder to vector of features
- for each data point in the current batch using pooling layer 1d
-- dense layer with linear activation function. use dropout
-"""
-
 
 class Transformer(nn.Module):
 
-    def __init__(self, n_feat, seq_len, d_model=256, n_head=8, n_layers=4, n_classes=2):
+    def __init__(self, seq_len: int, num_classes: int = 2, d_model=512, nhead=8, num_encoder_layers=6):
         super().__init__()
 
+        self.input_embedding_layer = nn.Linear(in_features=seq_len, out_features=d_model)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, batch_first=True)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+        self.output_layer = nn.Linear(in_features=d_model, out_features=num_classes)
         self.train_loss_curve = []
-
-        self.n_epochs = 0
-
-        self.input_embedding_layer = nn.Linear(
-            in_features=n_feat,
-            out_features=d_model
-        )
-
-        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_head)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
-
-        self.output_layer = nn.Linear(
-            in_features=d_model,
-            out_features=n_feat
-            )
-
-        self.output_layer2 = nn.Linear(
-                in_features=n_feat,
-                out_features=n_classes
-                )
+        self.num_epochs = 0
 
     def fit(self, x_train, y_train, lr=0.001, n_epochs=20):
-        
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         loss_fn = nn.CrossEntropyLoss()
-        
-        self.n_epochs = n_epochs        
+
+        self.num_epochs = n_epochs
         self.train_loss_curve = []
-        
+
         for n in range(n_epochs):
             y_pred = self.forward(x_train)
             loss = loss_fn(y_pred, y_train)
             self.train_loss_curve.append(loss.item())
             optimizer.zero_grad()
             loss.backward()
-            optimizer.step()        
+            torch.nn.utils.clip_grad_norm_(self.parameters(), 0.5)
+            optimizer.step()
 
     def evaluate(self, x_test, y_test):
 
-        y_pred = self.forward(x_test, predict=True)
+        y_pred = self.forward(x_test)
+        y_pred = torch.argmax(y_pred, dim=1)
+        accuracy = (y_pred == y_test).float().mean()
 
-        acc = (y_pred.round() == y_test).float().mean()
-        
-        acc = float(acc)
+        return y_pred, float(accuracy)
 
-        return acc
-        
-    def forward(self, x, predict=False):
+    def forward(self, x: torch.Tensor):
+        """
+        Arguments:
+            x: Tensor, shape ``[batch_size, seq_len]``
 
+        Return:
+             Tensor, shape ''[batch_size, num_classes]''
+        """
         x = self.input_embedding_layer(x)
-
         x = self.transformer_encoder(x)
-
         x = self.output_layer(x)
-
-        x = self.output_layer2(x)
-
-        if predict:
-            
-            return torch.argmax(x, dim=1)
 
         return x
