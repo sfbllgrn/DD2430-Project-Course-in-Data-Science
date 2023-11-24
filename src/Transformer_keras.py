06ef1269252c6054b5645d93baeb86be982912f0
aym1king <49128030+aym1king@users.noreply.github.com>
2023-11-24 15:45:33 +0100

    added learning rate scheduling

--- src/Transformer_keras.py
+++ src/Transformer_keras.py
@@ -90,8 +90,13 @@ class Classifier_TRANSFORMER:
         
         model = keras.Model(inputs=input_layer, outputs=output_layer)
 
-        lr_schedule = keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-2, decay_steps=10000, decay_rate=0.9)
-
+        initial_learning_rate = 0.1
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate,
+            decay_steps=100000,
+            decay_rate=0.96,
+            staircase=True)
+        
         model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
                        metrics=['accuracy', keras.metrics.AUC(name='f1_score')])
 
